1f53dac1fc4c2d13fbec658c6df11c4935706860
Jarrett Meyer <jarrettmeyer@gmail.com>
2016-09-14 13:59:51 -0400

    added comments to the infinite scroll mixin

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -27,40 +27,77 @@ App.IndexRoute = Ember.Route.extend({
 
 App.InfiniteScroll = Ember.Mixin.create({
 
-  hasNewRecords: true,
-  isLoadingData: false,
+  /**
+   * Value for number of records to fetch from the API at a time. This value may
+   * be overwritten in the controller for a smaller or larger fetch size.
+   */
   pageSize: 50,
-  threshold: 1.00,
 
-  loadData: function () {
+  /**
+   * Scrolling threshold as a proportion of the page before records are fetched.
+   * This value should be between 0.00 and 1.00.
+   */
+  threshold: 0.80,
+
+  /**
+   * This function must be overwritten in your controller. The function takes
+   * two arguments: `skip` and `take`. The function should return a promise of
+   * new records from your API.
+   */
+  loadData: function (/* skip, take */) {
     console.error('Must overwrite loadData in controller.');
   },
 
+  /**
+   * Value is `true` if new records were returned from the most recent API call.
+   * Otherwise, `false`.
+   */
+  _hasNewRecords: true,
+
+  /**
+   * Value is `true` if data is currently being loaded from the API. Otherwise,
+   * `false`.
+   */
+  _isLoadingData: false,
+
+  /**
+   * Function called after loading data is completed.
+   */
   _afterLoadingData: function (newRecords) {
-    this.set('isLoadingData', false);
+    this.set('_isLoadingData', false);
     if (newRecords.length === 0) {
-      this.set('hasNewRecords', false);
+      this.set('_hasNewRecords', false);
     }
     this.get('model').pushObjects(newRecords);
   },
 
+  /**
+   * Function called on mixin initialization.
+   */
   _onInit: function () {
     var _this = this;
+    // After rendering the screen is complete, set up the page scrolling.
     Ember.run.schedule('afterRender', function () {
       _this.get('_setupScrolling').bind(_this)();
     });
   }.on('init'),
 
+  /**
+   * Callback when scrolling threshold is reached.
+   */
   _onScroll: function () {
-    if (this.get('isLoadingData')) {
+    if (this.get('_isLoadingData')) {
       return;
     }
-    this.set('isLoadingData', true);
+    this.set('_isLoadingData', true);
     var recordCount = this.get('model.length');
     var pageSize = this.get('pageSize');
     return this.get('loadData')(recordCount, pageSize).then(this.get('_afterLoadingData').bind(this));
   },
 
+  /**
+   * Setup the page scrolling.
+   */
   _setupScrolling: function () {
     var _this = this;
     var threshold = this.get('_threshold');
